a4a282af43990378f1bb0d8e3814112c4129d67e
lijm <lijm@tsintergy.com>
2026-07-27 14:40:50 +0800

    feat(web): add exercise components barrel

---
+++ apps/web/src/components/exercises/index.ts
@@ -0,0 +1,12 @@
+export { ExerciseCard } from './ExerciseCard'
+export type { ExerciseCardProps } from './ExerciseCard'
+export { ExerciseDetailDialog } from './ExerciseDetailDialog'
+export type { ExerciseDetailDialogProps } from './ExerciseDetailDialog'
+export { ExerciseFormDialog } from './ExerciseFormDialog'
+export type { ExerciseFormDialogProps } from './ExerciseFormDialog'
+export {
+  CATEGORY_MAP,
+  DIFFICULTY_MAP,
+  CATEGORY_OPTIONS,
+  DIFFICULTY_OPTIONS,
+} from './constants'
